22779bb88fb38a1969337a7f7e649f1899a051c1
Borislav16 <borko.g16@gmail.com>
2024-11-02 10:44:16 +0200

    Initial commit
    Set up ASP.NET Core project with .NET 8, configured with authentication type: Individual Accounts.

---
+++ SocialNetworkForBooks/SocialNetworkForBooks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialNetworkForBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
