663112aac762f86d166e4cd2c2b66af99ac80ee8
Borislav16 <borko.g16@gmail.com>
2024-11-02 16:01:32 +0200

    Refactor services into extension methods
    Moved DbContext, Identity, and app services to extensions

---
+++ SocialNetworkForBooks/SocialNetworkForBooks/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SocialNetworkForBooks.Infrastructure.Data;
+using SocialNetworkForBooks.Infrastructure.Data.Models;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<SocialNetworkForBooksDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services
+                .AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<SocialNetworkForBooksDbContext>();
+
+            return services;
+        }
+    }
+}
