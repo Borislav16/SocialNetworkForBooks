8e8ed96f4163897b927d01e7ffc4f043da0ac07a
Borislav16 <borko.g16@gmail.com>
2024-11-02 16:40:39 +0200

    Add FirstName and LastName to ApplicationUser
    Updated ApplicationUser to include FirstName and LastName properties with validation.

--- SocialNetworkForBooks/SocialNetworkForBooks.Infrastructure/Data/Models/ApplicationUser.cs
+++ SocialNetworkForBooks/SocialNetworkForBooks.Infrastructure/Data/Models/ApplicationUser.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using SocialNetworkForBooks.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
 
 namespace SocialNetworkForBooks.Infrastructure.Data.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [MaxLength(DataConstants.UserFirstNameMaxLength)]
+        [Comment("The first name of the user, with a minimum length of 2 and a maximum length of 50 characters.")]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DataConstants.UserLastNameMaxLength)]
+        [Comment("The last name of the user, with a minimum length of 2 and a maximum length of 50 characters.")]
+        public string LastName { get; set; } = null!;
+
         [Comment("The date and time when the user was created.")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
